b588e956a86da7c6ec21935a52f9ca4e70ff00fb
mchehe <2596844541@qq.com>
2020-05-17 13:57:29 +0800

    xv

---
+++ api/xvideo.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const $ = require('cheerio');
+const request = require('request');
+const fs = require('fs');
+ 
+app.get('/api/xvideo', function (req, res) {
+	request({
+	    url: 'https://www.xvideos.com/?k=jk'+req.query.k,//请求路径
+	    method: "GET",//请求方式，默认为get
+	    headers: {//设置请求头
+	        "content-type": "application/json",
+	        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+	    },
+	}, function(error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	    	var data = [];
+	    	$('.mozaique .thumb-block',body).each(function(i, e) {
+	    		let info = {
+	    			title: $('.title a',e).attr('title'),
+	    			url: $('.title a',e).attr('href'),
+	    			img: $('img',e).attr('src'),
+	    			hd: $('.video-hd-mark',e).text(),
+	    			time: $('.duration',e).text()
+	    		};
+	    		data.push(info);
+		    });
+	    	res.json(data);
+	    }
+	}); 
+   // res.json();
+})
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
