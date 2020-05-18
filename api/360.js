a6f42d8844e5052bad8a6c5c52841a24fdb197ee
mchehe <2596844541@qq.com>
2020-05-19 01:43:56 +0800

    k允许跨域

--- api/360.js
+++ api/360.js
@@ -4,6 +4,17 @@ const $ = require('cheerio');
 const request = require('request');
 const fs = require('fs');
  
+// 跨域设置
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8")
+  next()
+})
+
 app.get('/api/360', function (req, res) {
 	request({
 	    url: 'https://www.360kan.com/dianying/list.php?year='+req.query.year+'&area='+req.query.area+'&act='+req.query.act+'&cat='+req.query.cat+'&pageno='+req.query.pageno,//请求路径
