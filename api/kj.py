a12dad70a22d94ceb857f7bd061dd21ca4e60741
mchehe <2596844541@qq.com>
2020-05-18 22:15:58 +0800

    py

--- api/kj.py
+++ api/kj.py
@@ -1,1 +1,9 @@
-print("hello word!")
+from sanic import Sanic
+from sanic.response import json
+app = Sanic()
+
+
+@app.route('/')
+@app.route('/<path:path>')
+async def index(request, path=""):
+    return json({'hello': path})
